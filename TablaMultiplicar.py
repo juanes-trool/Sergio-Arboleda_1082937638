963c9d1896af89c2d8f4829d0de5df83be733b00
Juan Gonzalez <juan.gonzalez45@usa.edu.co>
2026-03-09 07:30:31 -0500

    TablaMultiplicarRealizadas.py

---
+++ TablaMultiplicar.py
@@ -0,0 +1,6 @@
+for Tabla in range(1, 11):
+    print(f"Tabla del {Tabla}:")
+    for i in range(1, 11):
+        resultado = Tabla * i
+        print(f"{Tabla} x {i} = {resultado}")
+    print()  # Agrega una línea en blanco después de cada tabla 
